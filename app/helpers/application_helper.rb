936049a38e499be914646235d7a813a8a5a6a628
PaGul <spartaaa300@gmail.com>
2014-02-01 19:03:40 +0400

    Working spork+guard. Use first helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title (title)
+    btitle = "Ruby on Rails Tutorial Sample App"
+    if title.empty?
+      btitle
+    else
+      "#{btitle} | #{title}"
+    end
+  end
 end
